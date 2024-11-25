3fc243faa73e72bfd83eadbadb5cfdbcb2eef429
varun <varunkumar2004.vk@gmail.com>
2024-11-25 22:18:42 +0530

    first commit.

---
+++ app/src/main/java/com/varunkumar/expensetracker/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.varunkumar.expensetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.varunkumar.expensetracker.ui.theme.ExpenseTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExpenseTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
